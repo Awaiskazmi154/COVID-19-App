049c50500b3377f947c1ef1a549179fb9f479702
Awaiskazmi154 <75107223+Awaiskazmi154@users.noreply.github.com>
2021-01-01 20:18:02 +0500

    AsyncStorage Working Fine

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, ScrollView, Button, TouchableHighlight} from 'react-native';
-import { AsyncStorage } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Button,
+  TouchableHighlight,
+  AsyncStorage,
+} from 'react-native';
 import Constants from 'expo-constants';
-
 import { FontAwesome } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -24,7 +30,7 @@ const useStats = (url, keyObj) => {
     fetchData();
   }, []);
   return getStats;
-}
+};
 
 const WorldStatistics = (props) => {
   const stats = useStats(props.url1, props.keyObj1);
@@ -32,56 +38,64 @@ const WorldStatistics = (props) => {
 
   if (pstats && stats) var tpopulation = pstats.body.world_population;
 
-  if (!stats && !pstats) return <View style={styles.container}><Text style={styles.result}>Loading....</Text></View>;
+  if (!stats && !pstats)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.result}>Loading....</Text>
+      </View>
+    );
 
   return (
     <View style={styles.container}>
-      <View ><Text style={styles.result}>Total Population : {tpopulation}</Text></View>
-      <View >
-      <Text style={styles.result}>
-        Confirmed : {stats ? stats[0].confirmed : ''} Percentage :{' '}
-        {stats ? ((stats[0].confirmed / tpopulation) * 100).toFixed(5) : ''}
-         </Text>
+      <View>
+        <Text style={styles.result}>Total Population : {tpopulation}</Text>
       </View>
-      
-      <View >
-      <Text style={styles.result}>
-        Deaths : {stats ? stats[0].deaths : ''} Percentage :{' '}
-        {stats ? ((stats[0].deaths / tpopulation) * 100).toFixed(5) : ''}
-        </Text>
-      </View >
-      <View >
-      <Text style={styles.result}>
-        Recovered : {stats ? stats[0].recovered : ''} Percentage :{' '}
-        {stats ? ((stats[0].recovered / tpopulation) * 100).toFixed(5) : ''}
+      <View>
+        <Text style={styles.result}>
+          Confirmed : {stats ? stats[0].confirmed : ''} Percentage :{' '}
+          {stats ? ((stats[0].confirmed / tpopulation) * 100).toFixed(5) : ''}
         </Text>
       </View>
-      <View >
-      <Text style={styles.result}>
-        Critical Cases : {stats ? stats[0].critical : ''} Percentage :{' '}
-        {stats ? ((stats[0].critical / tpopulation) * 100).toFixed(5) : ''}
-        </Text>
-      </View>
-      
-      <View style={styles.result}><Text style={styles.result}>Last Updated : {stats ? stats[0].lastUpdate : ''}</Text></View>
-            
-      <View style={styles.buttons}>
-     
-      </View>
-    </View> 
-  );
-}
 
+      <View>
+        <Text style={styles.result}>
+          Deaths : {stats ? stats[0].deaths : ''} Percentage :{' '}
+          {stats ? ((stats[0].deaths / tpopulation) * 100).toFixed(5) : ''}
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.result}>
+          Recovered : {stats ? stats[0].recovered : ''} Percentage :{' '}
+          {stats ? ((stats[0].recovered / tpopulation) * 100).toFixed(5) : ''}
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.result}>
+          Critical Cases : {stats ? stats[0].critical : ''} Percentage :{' '}
+          {stats ? ((stats[0].critical / tpopulation) * 100).toFixed(5) : ''}
+        </Text>
+      </View>
+
+      <View style={styles.result}>
+        <Text style={styles.result}>
+          Last Updated : {stats ? stats[0].lastUpdate : ''}
+        </Text>
+      </View>
+
+      <View style={styles.buttons}></View>
+    </View>
+  );
+};
 
 const CustomButton = (props) => {
   if (props.pressed) {
     var btnColor = 'grey';
-    var text = 'Saved'
+    var text = 'Saved';
   } else {
     btnColor = '#F194FF';
-    text = 'Save'
+    text = 'Save';
   }
-  return ( 
+  return (
     <TouchableHighlight
       activeOpacity={0.5}
       onPress={props.onPressEvent}
@@ -101,102 +115,119 @@ const CustomButton = (props) => {
 };
 
 const Countries = (props) => {
-
   const cnames = useStats(props.url2, props.keyObj2);
-  const [getFavourites, setFavourites] = useState([]);
+  const [getList, setList] = useState([]);
+  const [getdata, setdata] = useState({});
 
+  useEffect(() => {
+    var country = getdata.country;
+    var code = getdata.code;
+    const saveToFav = async (country, code) => {
+      await AsyncStorage.setItem(country, code);
+      console.log(country + ' stored');
+      if (!getList.includes(country)) setList([...getList, country]);
+    };
+    saveToFav(country, code);
+  }, [getdata]);
 
-props.navigation.setOptions({
-    headerRight: () => (
-      <View style={{ paddingRight: 10 }}>
-        <FontAwesome
-          name="star"
-          size={24}
-          color="black"
-          onPress={() => props.navigation.navigate('FavCountriesScreen', {getFavourites})}
-        />
+  useEffect(() => {
+    const load = async () => {
+      setList(await AsyncStorage.getAllKeys());
+    };
+    load();
+  }, [getList]);
+
+  if (!cnames)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.result}>Loading....</Text>
       </View>
-    ),
-  });
-
-
-const saveToFav =  (country) => {
-
-if(!getFavourites.includes(country))
-setFavourites([...getFavourites,country])
-
-}
-
-useEffect(() => {
-  if(!getFavourites)
-props.navigation.navigate('FavCountriesScreen', {getFavourites} )
-}, []);
-
-  if (!cnames) return <View style={styles.container}><Text style={styles.result}>Loading....</Text></View>;
+    );
 
   return (
     <View>
       <ScrollView>
         {Object.entries(cnames.body.countries).map(([code, country]) => (
           <View style={styles.row}>
-          <Text style={styles.result}
-            onPress={() =>
-              props.navigation.navigate('CountriesDetailsScreen', {
-                country,
-              }) 
-            }>   
-            {country} 
-          </Text>
-          <CustomButton 
-        
-        text={'Save'}
-        textSize={12}
-        textColor={'white'}
-        onPressEvent={()=>saveToFav(country)}
-        pressed={getFavourites.includes(country) ? true: false}
-      />
-</View>
-        ))}  
-        <Button
-          title="fav"
-          onPress={() => props.navigation.navigate('FavCountriesScreen', {getFavourites})}
-        />
+            <Text
+              style={styles.result}
+              onPress={() =>
+                props.navigation.navigate('CountriesDetailsScreen', { country })
+              }>
+              {country}
+            </Text>
+            <CustomButton
+              text={'Save'} 
+              textSize={12}
+              textColor={'white'}
+              onPressEvent={() => setdata({ country, code })}
+              pressed={getList.includes(country) ? true : false}
+            />
+          </View>
+        ))}
       </ScrollView>
     </View>
   );
-}
-
+};
 
 const FavCountries = (props) => {
+  const [getList, setList] = useState([]);
+  const [getdata, setdata] = useState();
 
-  const cnames = useStats(props.url2, props.keyObj2);
+  props.navigation.setOptions({
+    headerRight: () => (
+      <View style={{ paddingRight: 10 }}>
+        <FontAwesome
+          name="recycle"
+          size={24}
+          color="black"
+          onPress={() => clear()}
+        />
+      </View>
+    ),
+  });
 
-const [getcList, setcList] = useState(
-    props.route.params.getFavourites
-  );
-  
+  const clear = async () => {
+    await AsyncStorage.clear();
+    setList([]);
+    console.log('cleared');
+  };
 
-  if (!cnames) return <View style={styles.container}><Text style={styles.result}>Loading....</Text></View>;
+  useEffect(() => {
+    const del = async (getdata) => {
+      await AsyncStorage.removeItem(getdata);
+      console.log(getdata + ' deleted');
+      setList(() => getList.filter((item) => item != getdata));
+    };
+    del(getdata);
+  }, [getdata]);
+
+  useEffect(() => {
+    const load = async () => {
+      setList(await AsyncStorage.getAllKeys());
+    };
+    load();
+  }, [getList]);
 
   return (
     <View>
       <ScrollView>
-        {getcList? getcList.map((country) => (
-          <View style={styles.container}>
-          <Text style={styles.result}
-            onPress={() =>
-              props.navigation.navigate('CountriesDetailsScreen', {
-                country,
-              })
-            }>
-            {country}
-          </Text>
-</View>
-        )):""}
+        {getList.map((country) => (
+          <View style={styles.row}>
+            <Text
+              style={styles.result}
+              onPress={() =>
+                props.navigation.navigate('CountriesDetailsScreen', { country })
+              }>
+              {country}
+            </Text>
+            <Button title="X" onPress={() => setdata(country)} />
+          </View>
+        ))}
       </ScrollView>
     </View>
   );
-}
+};
 
 const CountryDetailsScreen = (props) => {
   if (props.route.params.country != 'United States') {
@@ -216,7 +247,12 @@ const CountryDetailsScreen = (props) => {
 
   if (pdata) console.log(pdata);
 
-  if (!pdata) return <View style={styles.container}><Text style={styles.results}>Loading....</Text></View>;
+  if (!pdata)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.results}>Loading....</Text>
+      </View>
+    );
   return (
     <View style={styles.container}>
       <ScrollView style={styles.sView}>
@@ -255,146 +291,139 @@ const CountryDetailsScreen = (props) => {
       </ScrollView>
       <View style={styles.buttons}>
         <Button title="Back" onPress={() => props.navigation.goBack()} />
-        
       </View>
     </View>
   );
-}
+};
 
 const Stack = createStackNavigator();
 
 const StackNavigator1 = () => {
   return (
-<Stack.Navigator initialRouteName={'CountriesScreen'}>
-        
-        <Stack.Screen
-          name="CountriesScreen"
-          options={{ title: 'Countries', headerTitleAlign: 'center' }}>
-          {(props) => (
-            <Countries
-              {...props}
-             
-              url2={'https://world-population.p.rapidapi.com/allcountriesname'}
-              keyObj2={{
-                method: 'GET',
-                headers: {
-                  'x-rapidapi-key':
-                    '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
-                  'x-rapidapi-host': 'world-population.p.rapidapi.com',
-                },
-              }}
-            />
-          )}
-        </Stack.Screen>
-        <Stack.Screen
-          name="FavCountriesScreen"
-          options={{ title: 'Favourite Countries', headerTitleAlign: 'center' }}>
-          {(props) => (
-            <FavCountries
-              {...props}
-             
-              url2={'https://world-population.p.rapidapi.com/allcountriesname'}
-              keyObj2={{
-                method: 'GET',
-                headers: {
-                  'x-rapidapi-key':
-                    '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
-                  'x-rapidapi-host': 'world-population.p.rapidapi.com',
-                },
-              }}
-            />
-          )}
-        </Stack.Screen>
-        <Stack.Screen
-          name="CountriesDetailsScreen"
-          options={{ title: 'Country Details', headerTitleAlign: 'center' }}>
-          {(props) => (
-            <CountryDetailsScreen
-              {...props}
-              url1={'https://covid-19-data.p.rapidapi.com/country?name='}
-              url2={
-                'https://world-population.p.rapidapi.com/population?country_name='
-              }
-              keyObj1={{
-                method: 'GET',
-                headers: {
-                  'x-rapidapi-key':
-                    '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
-                  'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
-                },
-              }}
-              keyObj2={{
-                method: 'GET',
-                headers: {
-                  'x-rapidapi-key':
-                    '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
-                  'x-rapidapi-host': 'world-population.p.rapidapi.com',
-                },
-              }}
-            />
-          )}
-        </Stack.Screen>
-      </Stack.Navigator>
-  )
-  }
+    <Stack.Navigator initialRouteName={'CountriesScreen'}>
+      <Stack.Screen
+        name="CountriesScreen"
+        options={{ title: 'Countries', headerTitleAlign: 'center' }}>
+        {(props) => (
+          <Countries
+            {...props}
+            url2={'https://world-population.p.rapidapi.com/allcountriesname'}
+            keyObj2={{
+              method: 'GET',
+              headers: {
+                'x-rapidapi-key':
+                  '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
+                'x-rapidapi-host': 'world-population.p.rapidapi.com',
+              },
+            }}
+          />
+        )}
+      </Stack.Screen>
 
+      <Stack.Screen
+        name="CountriesDetailsScreen"
+        options={{ title: 'Country Details', headerTitleAlign: 'center' }}>
+        {(props) => (
+          <CountryDetailsScreen
+            {...props}
+            url1={'https://covid-19-data.p.rapidapi.com/country?name='}
+            url2={
+              'https://world-population.p.rapidapi.com/population?country_name='
+            }
+            keyObj1={{
+              method: 'GET',
+              headers: {
+                'x-rapidapi-key':
+                  '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
+                'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+              },
+            }}
+            keyObj2={{
+              method: 'GET',
+              headers: {
+                'x-rapidapi-key':
+                  '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
+                'x-rapidapi-host': 'world-population.p.rapidapi.com',
+              },
+            }}
+          />
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
+};
 
 const StackNavigator2 = () => {
-  const stack =  StackNavigator1;
-  return ( 
-<Stack.Navigator initialRouteName={'FavCountriesScreen'}>    
-      {stack}
-      </Stack.Navigator>
-   )
-  }
+  return (
+    <Stack.Navigator initialRouteName={'FavCountriesScreen'}>
+      <Stack.Screen
+        name="FavCountriesScreen"
+        options={{ title: 'Favourite Countries', headerTitleAlign: 'center' }}>
+        {(props) => (
+          <FavCountries
+            {...props}
+            url2={'https://world-population.p.rapidapi.com/allcountriesname'}
+            keyObj2={{
+              method: 'GET',
+              headers: {
+                'x-rapidapi-key':
+                  '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
+                'x-rapidapi-host': 'world-population.p.rapidapi.com',
+              },
+            }}
+          />
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
+};
 
- 
 const Drawer = createDrawerNavigator();
 
- const DrawerNavigator = () => {
+const DrawerNavigator = () => {
   return (
-      <Drawer.Navigator initialRouteName="World Statistics">
-        <Drawer.Screen name="World Statistics" options={{ headerShown: true }}>
-          {(props) => (
-            <WorldStatistics
-              {...props}
-              url1={'https://covid-19-data.p.rapidapi.com/totals?format=json'}
-              keyObj1={{
-                method: 'GET',
-                headers: {
-                  'x-rapidapi-key':
-                    '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
-                  'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
-                },
-              }}
-              url2={'https://world-population.p.rapidapi.com/worldpopulation'}
-              keyObj2={{
-                method: 'GET',
-                headers: {
-                  'x-rapidapi-key':
-                    '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
-                  'x-rapidapi-host': 'world-population.p.rapidapi.com',
-                },
-              }}
-            />
-          )}
-        </Drawer.Screen>
-        <Drawer.Screen name="Country Statistics" component={StackNavigator1} />
-        <Drawer.Screen name="Favourite Countries" component={StackNavigator1} />
-      </Drawer.Navigator>
+    <Drawer.Navigator initialRouteName="World Statistics">
+      <Drawer.Screen name="World Statistics" options={{ headerShown: true }}>
+        {(props) => (
+          <WorldStatistics
+            {...props}
+            url1={'https://covid-19-data.p.rapidapi.com/totals?format=json'}
+            keyObj1={{
+              method: 'GET',
+              headers: {
+                'x-rapidapi-key':
+                  '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
+                'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+              },
+            }}
+            url2={'https://world-population.p.rapidapi.com/worldpopulation'}
+            keyObj2={{
+              method: 'GET',
+              headers: {
+                'x-rapidapi-key':
+                  '19449ad71bmsh6ef1edc74267936p131d7ajsn96f451de877a',
+                'x-rapidapi-host': 'world-population.p.rapidapi.com',
+              },
+            }}
+          />
+        )}
+      </Drawer.Screen>
+      <Drawer.Screen name="Country Statistics" component={StackNavigator1} />
+      <Drawer.Screen name="Favourite Countries" component={StackNavigator2} />
+    </Drawer.Navigator>
   );
-}
- 
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigator/>
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
- container: {
+  container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
@@ -405,14 +434,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
-   saveButton: {
+  saveButton: {
     backgroundColor: '#F194FF',
     borderRadius: 20,
     padding: 10,
     margin: 5,
     elevation: 2,
   },
-   result: {
+  result: {
     textAlign: 'center',
     fontWeight: '500',
     fontSize: 15,
@@ -423,5 +452,5 @@ const styles = StyleSheet.create({
     fontSize: 15,
     padding: '5%',
     justifyContent: 'space-between',
-  }
+  },
 });
